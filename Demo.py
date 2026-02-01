242ba3d882f85dd0673bb52f67abdcf81a03e608
Ravikumarallam <ravikumarallam0222@gmail.com>
2026-02-01 14:09:31 +0530

    Moving code changes to Addition.py file

--- Demo.py
+++ Demo.py
@@ -1,21 +1,3 @@
 
 #for including single quote ' with the help of --> " "
 print("Ravi's book")
-
-#remove the special meaning of \n --> r (raw string)
-b= r'c:\docs\ravi_new\new_code'
-print(b)
-
-
-#multi print lly for + - * / // (Floor division) ** (power off) % (output will be remainder) _ (under score for previous output)
-a='ravi'
-print(a*5)
-
-num1 = 8
-num2 = 2
-
-print(num1+num2, num1-num2, num1*num2, num1/num2, num1//num2, num1**num2, num1%num2)
-
-#indexing [a:b] a--> starting, b --> ending
-name = 'RAVIALLAM'
-print(name[0], name[-1], name[0:2], name[1:], name[:4], name[::-1])
