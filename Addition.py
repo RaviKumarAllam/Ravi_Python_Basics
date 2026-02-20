f47d78436ad6d8614dd18554f1685832976595a7
Ravi Kumar Allam <104352072+RaviKumarAllam@users.noreply.github.com>
2026-02-20 22:57:06 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -7,6 +7,10 @@ print(a + b + c)
 #remove the special meaning of \n --> r (raw string)
 
 #multi print lly for + - * / // (Floor division) ** (power off) % (output will be reminder) _ (underscore for previous output)
+x=10
+y=x*2
+z= _+15
+print(z)
 a='ravi'
 print(a*5)
 
